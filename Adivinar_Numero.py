9dfeca0e8fb2a7f62b3a81ee02b9d8331b6e4df6
pablomarquezuax <pmarlop@myuax.com>
2023-11-20 17:02:10 +0100

    Update README + Borrar  python y rehacer bien

--- Adivinar_Numero.py
+++ Adivinar_Numero.py
@@ -1,20 +1,100 @@
 import random
 
-numero_aleatorio = random.randint(0, 99)
+#Abrir el diccionario para guargar los nombres de los jugadores y sus puntuaciones (numero de intentos)
+MEJORES_PUNTUACIONES = {}
 
-intentos = 0
+#Esta función carga las mejores puntuaciones ya guardadas o crea un diccionario nuevo si no hay registros.
+def cargar_mejores_puntuaciones():
+    return {}
 
-while True:
-    intento = int(input("Adivina el número: "))
-    intentos += 1
+# Esta función muestra un menú de niveles para que elija uno. 
+# Devuelve el rango del nivel seleccionado y un booleano que indica si se debe ofrecer ayuda durante el juego.
+def seleccionar_nivel():
+    print("Selecciona un nivel de dificultad:")
+    print("1. Nivel Simple (0-100)")
+    print("2. Nivel Intermedio (0-1,000)")
+    print("3. Nivel Avanzado (0-1,000,000)")
+    print("4. Nivel Experto (0-1,000,000,000,000)")
 
-    if intento == numero_aleatorio:
-        print("¡Lo has adivinado!")
-        print("Enhorabuena! Lo has conseguido adivinar en", intentos, "intentos!!")
-        break
-    
-    elif intento > numero_aleatorio:
-        print("Te has pasado, inténtalo otra vez.")
+    opcion = int(input("Ingresa el número del nivel que prefieres: "))
 
-    elif intento < numero_aleatorio:
-        print("Te has quedado corto, inténtalo otra vez.")
+    if opcion == 1:
+        return 0, 100, True
+    elif opcion == 2:
+        return 0, 1000, True
+    elif opcion == 3:
+        return 0, 1000000, True
+    elif opcion == 4:
+        return 0, 1000000000000, True
+    else:
+        print("La opción que has seleccionado no es válida. Estás jugando en Nivel Simple por defecto.")
+        return 0, 100, True
+
+# Genera un número secreto aleatorio dentro del rango especificado.
+def generar_numero_secreto(minimo, maximo):
+    return random.randint(minimo, maximo)
+
+
+# Muestra el rango actual del número secreto si el usuario ha solicitado ayuda.
+# Si el input es cualquier cosa que no sea ¨si¨, el programa entiende que el jugador no necesita ayuda.
+def ofrecer_ayuda(minimo, maximo):
+    ayuda = input("¿Quieres recibir ayuda? (si/no): ")
+    if ayuda == "si":
+        print(f"Número mínimo: {minimo}, Número máximo: {maximo}")
+
+
+# Muestra las mejores puntuaciones almacenadas hasta el momento.
+def mostrar_mejores_puntuaciones():
+    print("Mejores Puntuaciones:")
+    for nombre, intentos in MEJORES_PUNTUACIONES.items():
+        print(f"{nombre}: {intentos} intentos")
+
+
+# Compara el número adivinado con el número secreto. 
+# Devuelve un string que indica si el número es demasiado bajo, alto o correcto. 
+# (Ese string luego se inserta en la respuesta del programa al jugador.)
+def comparar_numeros(numero, secreto):
+    if numero < secreto:
+        return "bajo"
+    elif numero > secreto:
+        return "alto"
+    else:
+        return "correcto"
+
+
+# La función principal del juego. 
+# Inicia el juego 
+# Solicita al usuario que elija un nivel 
+# Genera un número secreto y maneja el juego hasta que el usuario adivina el número.
+def jugar():
+    minimo, maximo, ayuda = seleccionar_nivel()
+    numero_secreto = generar_numero_secreto(minimo, maximo)
+    intentos = 0
+
+    print(f"¡Bienvenido al juego de adivinar el número! Nivel: {minimo}-{maximo}")
+
+    while True:
+        if ayuda:
+            ofrecer_ayuda(minimo, maximo)
+
+        intento = int(input("Adivina el número: "))
+        intentos += 1
+
+        resultado = comparar_numeros(intento, numero_secreto)
+        print(f"Tu intento {intento} es {resultado}. Inténtalo de nuevo!")
+
+        if resultado == "correcto":
+            print(f"¡Felicidades! Adivinaste el número en {intentos} intentos.")
+            nombre_jugador = input("Ingresa tu nombre para guardar tu puntuación: ")
+            MEJORES_PUNTUACIONES[nombre_jugador] = intentos
+            break
+
+    mostrar_mejores_puntuaciones()
+
+
+# Ejecuta automaticamente la función de ¨jugar()¨, así como el diccionario de mejores puntuaciones.
+# Se utiliza para determinar si el script de Python se está ejecutando como un programa principal o si se está importando como un módulo en otro script.
+# En este caso, se trata del programa principal y no de un módulo del mismo.
+if __name__ == "__main__":
+    MEJORES_PUNTUACIONES = cargar_mejores_puntuaciones()
+    jugar()
--- README.md
+++ README.md
@@ -7,3 +7,27 @@ Puedes ver este repositorio en linea en el siguiente enlace: https://github.com/
 **Instrucciones de la tarea:** Va a crear un programa para terminal que va a escoger un número aleatoriamente, entre 0 y 99, y a continuación, le va a pedir al usuario que adivine este número.
 
 Tras cada intento, le responderá indicándole si se ha quedado corto o se ha pasado, hasta que encuentre el número. Entonces, mostrará el número de intentos que han hecho falta para encontrar este número y el programa se terminará.
+
+**Intrucciones para terminar el juego:**
+
+- ***Paso 1:***
+
+El primer cambio consiste en crear un menú que permita seleccionar un nivel de dificultad: nivel simple (entre 0 y 100), nivel intermedio (entre 0 y 1.000), nivel avanzado (entre 0 y 1.000.000) y nivel experto (entre 0 y 1.000.000.000.000).
+
+El jugador podrá escoger de manera sencilla su nivel, por ejemplo entre 1 y 4, y los valores mínimo y máximo se determinarán automáticamente.
+
+De manera opcional puede, sea cual sea el nivel, proponer al jugador una ayuda (mostrar el número mínimo y máximo deducidos de las anteriores entradas) o rechazarla.
+
+Puede crear una función para gestionar este menú, que incluirá en un nuevo módulo entrada.menu. También debe crear nuevas funciones en el módulo juego y revisar la función jugar.
+
+
+- ***Paso 2:***
+
+De manera opcional puede, sea cual sea el nivel, proponer al jugador una ayuda (mostrar el número mínimo y máximo deducidos de las anteriores entradas) o rechazarla.
+
+Puede crear una función para gestionar este menú, que incluirá en un nuevo módulo entrada.menu. También debe crear nuevas funciones en el módulo juego y revisar la función jugar.
+
+
+- ***Paso 3:***
+
+Al final de una partida ganada, puede también pedir al jugador su nombre y guardarlo en la tabla de mejores puntuaciones. En primer lugar, esta tabla se creará al inicio del programa y los datos se perderán una vez se cierre.
